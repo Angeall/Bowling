66568fdba99aa9e6e7ea05e1a90b14f55ae6482a
Anthony Rouneau <angeal1105@gmail.com>
2017-05-24 17:19:14 +0200

    Changed default player name

--- main_game.py
+++ main_game.py
@@ -3,7 +3,7 @@ import sys
 from bowling.game import BowlingGame
 
 if __name__ == '__main__':
-    players = ["TestPlayer"]
+    players = ["Player1"]
     if len(sys.argv) > 1:
         players = sys.argv[1:]
     game = BowlingGame(players)
